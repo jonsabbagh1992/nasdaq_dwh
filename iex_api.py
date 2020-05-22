a841bf46d39364152b4812fc929d80b30eec5e46
Jonathan Sabbagh <jb.sabbagh1@gmail.com>
2020-05-22 13:46:16 -0400

    refactor: removed API functionality to advanced stats
    Removed functionality to request advanced stats as it was creating issues when loading to postgres.
    Will look into reimplementing in the future.

--- iex_api.py
+++ iex_api.py
@@ -10,7 +10,7 @@ class IEXmanager:
         self.token   = config.get("API", 'TOKEN')
         self.domain  = config.get('API', 'DOMAIN')
         self.version = config.get('API', 'VERSION')
-        self.api_types = ['company', 'stats', 'advanced-stats']
+        self.api_types = ['company', 'stats']
     
     def api_type_exists(self, api_type):
         return api_type in self.api_types
@@ -18,13 +18,15 @@ class IEXmanager:
     def get_symbol_data(self, symbol, api_type):
         SUCCESS_CODE = 200
         if self.api_type_exists(api_type):
-            api_https = f"https://{self.domain}/{self.version}/stock/{symbol}/{self.api_type}?token={self.token}"
+            api_https = f"https://{self.domain}/{self.version}/stock/{symbol}/{api_type}?token={self.token}"
             response = requests.get(api_https)
             
             if response.status_code == SUCCESS_CODE:
                 json_data = json.loads(response.text)
-                if api_type == 'advanced-stats':
+                if api_type == 'stats':
                     json_data['symbol'] = symbol
+                elif api_type == 'company':
+                    del json_data['tags'] # Removed this value because it is not currently desired
             else:
                 raise requests.RequestException(f"Request failed with the following status code {response.status_code}")
             
